ac86d43b0003850dccb4eae76789b02a72806258
rievant77 <vans.imey76@gmail.com>
2019-07-28 14:10:09 +0700

    Perubahan Coding Cetak

--- 1/lihat.php
+++ 1/lihat.php
@@ -15,123 +15,147 @@ $angkutan = mysqli_fetch_array ($queryangkutan);
 ?>
 
 <script type="text/javascript">
- $(function(){
-  $(".datepicker").datepicker({
-      format: 'yyyy-mm-dd',
-      autoclose: true,
-      todayHighlight: true,
-  });
- });
+	$(function () {
+		$(".datepicker").datepicker({
+			format: 'yyyy-mm-dd',
+			autoclose: true,
+			todayHighlight: true,
+		});
+	});
 </script>
 
 <!DOCTYPE html>
 <html>
- <head>
-    <meta charset="utf-8">
-    <title>Terminal Pasirhayam</title>
+
+<head>
+	<meta charset="utf-8">
+	<title>Terminal Pasirhayam</title>
 	<!-- Library CSS -->
 	<?php
 		include "bundle_css.php";
 	?>
-  </head>
-  <body class="hold-transition skin-blue sidebar-mini">
-    <div class="wrapper">
-      <?php
+</head>
+
+<body class="hold-transition skin-blue sidebar-mini">
+	<div class="wrapper">
+		<?php
         include 'content_header.php';
        ?>
-      <!-- Left side column. contains the logo and sidebar -->
-      <aside class="main-sidebar">
-        <!-- sidebar: style can be found in sidebar.less -->
-        <section class="sidebar">
-          <!-- Sidebar user panel -->
-          <div class="user-panel">
-            <div class="pull-left image">
-              <p></p>
-            </div>
-          </div><!-- sidebar menu: : style can be found in sidebar.less -->
-          <ul class="sidebar-menu">
-              <li class="header"><h4><b><center>Menu Panel</center></b></h4></li>
-              <li><a href="index.php"><i class="fa fa-home"></i><span>Dashboard</span></a></li>
-			        <li class="active"><a href="dosen.php"><i class="fa fa-user"></i><span>Angkutan</span></a></li>
-			        <li class="active"><a href="#"><i class="fa fa-user"></i><span>Report</span></a></li>
+		<!-- Left side column. contains the logo and sidebar -->
+		<aside class="main-sidebar">
+			<!-- sidebar: style can be found in sidebar.less -->
+			<section class="sidebar">
+				<!-- Sidebar user panel -->
+				<div class="user-panel">
+					<div class="pull-left image">
+						<p></p>
+					</div>
+				</div><!-- sidebar menu: : style can be found in sidebar.less -->
+				<ul class="sidebar-menu">
+					<li class="header">
+						<h4><b>
+								<center>Menu Panel</center>
+							</b></h4>
+					</li>
+					<li><a href="index.php"><i class="fa fa-home"></i><span>Dashboard</span></a></li>
+					<li class="active"><a href="dosen.php"><i class="fa fa-user"></i><span>Angkutan</span></a></li>
+					<li class="active"><a href="#"><i class="fa fa-user"></i><span>Report</span></a></li>
 					<li><a href="#"><i class="fa fa-user-circle-o"></i><span>User</span></a></li>
-        </section>
-        <!-- /.sidebar -->
-      </aside>
+			</section>
+			<!-- /.sidebar -->
+		</aside>
 
-      <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-          <h1>
-            Terminal
-          </h1>
-          <ol class="breadcrumb">
-            <li><i class="fa fa-user"></i> Terminal</li>
-          </ol>
-        </section>
+		<!-- Content Wrapper. Contains page content -->
+		<div class="content-wrapper">
+			<!-- Content Header (Page header) -->
+			<section class="content-header">
+				<h1>
+					Terminal
+				</h1>
+				<ol class="breadcrumb">
+					<li><i class="fa fa-user"></i> Terminal</li>
+				</ol>
+			</section>
 
-        <!-- Main content -->
-        <section class="content">
-          <div class="row">
-            <div class="col-xs-12">
-              <div class="box">
-                <div class="box-header">
+			<!-- Main content -->
+			<section class="content">
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="box">
+							<div class="box-header">
 
-              		<table border="10" width="100%" class="table table-bordered table-striped table-scalable">
-              			<thead>
-              				<tr>                                      
-		                    	<td>No. Kendaraan</td>
-		                    	<td><center>:</center></td>
-		                    	<td><?php echo"$angkutan[noken]";?></td>
-		                    </tr>
-		                    <tr>                                      
-		                    	<td>Perusahaan PO</td>
-		                    	<td><center>:</center></td>
-		                    	<td><?php echo"$angkutan[po]";?></td>
-		                    </tr>
-		                    <tr>                                      
-		                    	<td>Nama Sopir</td>
-		                    	<td><center>:</center></td>
-		                    	<td><?php echo"$angkutan[supir]";?></td>
-		                    </tr>
-		                   <tr>                                      
-		                    	<td>Kartu Pengawas</td>
-		                    	<td><center>:</center></td>
-		                    	<td><?php echo"$angkutan[kp]";?></td>
-		                    </tr>
-		                    <tr>                                      
-		                    	<td>Masa Berlaku Uji</td>	
-		                    	<td><center>:</center></td>
-		                    	<td><?php echo"$angkutan[uji]";?></td>
-		                    </tr>
-		                    <tr>                                      
-		                    	<td>Penumpang Naik</td>
-		                    	<td><center>:</center></td>
-		                    	<td><?php echo"$angkutan[naik]";?></td>
-		                    </tr>
-		                    <tr>                                      
-		                    	<td>Penumpang Turun</td>
-		                    	<td><center>:</center></td>
-		                    	<td><?php echo"$angkutan[turun]";?></td>
-		                    </tr>
-		                    <tr>                                      
-		                    	<td>Kelengkapan</td>
-		                    	<td><center>:</center></td>
-		                    	<td><?php echo"$angkutan[kel]";?></td>
-		                    </tr>
+								<table border="10" width="100%"
+									class="table table-bordered table-striped table-scalable">
+									<thead>
+										<tr>
+											<td>No. Kendaraan</td>
+											<td>
+												<center>:</center>
+											</td>
+											<td><?php echo"$angkutan[noken]";?></td>
+										</tr>
+										<tr>
+											<td>Perusahaan PO</td>
+											<td>
+												<center>:</center>
+											</td>
+											<td><?php echo"$angkutan[po]";?></td>
+										</tr>
+										<tr>
+											<td>Nama Sopir</td>
+											<td>
+												<center>:</center>
+											</td>
+											<td><?php echo"$angkutan[supir]";?></td>
+										</tr>
+										<tr>
+											<td>Kartu Pengawas</td>
+											<td>
+												<center>:</center>
+											</td>
+											<td><?php echo"$angkutan[kp]";?></td>
+										</tr>
+										<tr>
+											<td>Masa Berlaku Uji</td>
+											<td>
+												<center>:</center>
+											</td>
+											<td><?php echo"$angkutan[uji]";?></td>
+										</tr>
+										<tr>
+											<td>Penumpang Naik</td>
+											<td>
+												<center>:</center>
+											</td>
+											<td><?php echo"$angkutan[naik]";?></td>
+										</tr>
+										<tr>
+											<td>Penumpang Turun</td>
+											<td>
+												<center>:</center>
+											</td>
+											<td><?php echo"$angkutan[turun]";?></td>
+										</tr>
+										<tr>
+											<td>Kelengkapan</td>
+											<td>
+												<center>:</center>
+											</td>
+											<td><?php echo"$angkutan[kel]";?></td>
+										</tr>
 
 
-		                     <p class="page-header"><a href="dosen.php"><button class="btn btn-default">Kembali</button></a> 
-		                    
-              			</thead>
-			     
+										<p class="page-header"><a href="dosen.php"><button
+													class="btn btn-default">Kembali</button></a>
+
+									</thead>
+
+							</div>
+						</div>
+					</div>
 				</div>
-			</div>
-			</div>
-			</div>
-		</section>
-	</div>
+			</section>
+		</div>
 	</div>
 
 	<?php
@@ -139,5 +163,6 @@ $angkutan = mysqli_fetch_array ($queryangkutan);
 		include "bundle_script.php";
 	?>
 
-  </body>
+</body>
+
 </html>
--- 1/report-filter.php
+++ 1/report-filter.php
@@ -1,8 +1,56 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+session_start();
+error_reporting(0);
+include "../koneksi.php";
 
-$mpdf = new \Mpdf\Mpdf();
-$mpdf->WriteHTML('<h1>Hello world!</h1>');
-$mpdf->Output('report.pdf');
+$conten ='
+<style>
+.table { border-collapse:collapse; }
+.table th { padding:8px 5px; background-color:#f60; color#fff; }
+</style>
+';
+
+$conten .= '
+<page>
+<div style="padding:4mm; border:1px solid;" align="center">
+<span style="font-size:25px;">Laporan Data Kendaraan</span>
+</div>
+<p>
+<table border="1px" class="table" align="center">
+<tr>
+<th>No</th>
+<th>No. Kendaraan</th>
+<th>Perusahaan PO</th>
+<th>Nama Sopir</th>
+<th>Kartu Pengawas</th>
+<th>Masa Berlaku Uji</th>
+</tr>';
+$no = 1;
+$tampil = $angkutan->tampil();
+while ($data = $tampil->fetch_object()) {
+	$conten .= '	
+<tr>
+<td align="center">'.$no++.'</td>
+<td>'.$data->noken.'</td>
+<td>'.$data->po.'</td>
+<td>'.$data->supir.'</td>
+<td>'.$data->kp.'</td>
+<td>'.$data->uji.'</td>
+</tr>
+
+	';
+}
+$conten .='
+</table>
+</p>
+
+</page>';
+require __DIR__.'/../vendor/autoload.php';
+
+use Spipu\Html2Pdf\Html2Pdf;
+
+$html2pdf = new Html2Pdf('P','A4','en');
+$html2pdf->writeHTML($conten);
+$html2pdf->output('laporan.pdf');
 ?>
