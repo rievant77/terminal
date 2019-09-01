b3555a622ccdeb8df6a68502b3cca4a4ccfe2925
rievant77 <vans.imey76@gmail.com>
2019-09-01 21:18:38 +0700

    menambah kwt.php

--- 1/kwt.php
+++ 1/kwt.php
@@ -1,29 +1,85 @@
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Cetak Semua SK</title>
+<style>
+    @media print
+{    
+    .no-print, .no-print *
+    {
+        display: none !important;
+    }
+}
+</style>
+</head>
+<body>
+<div align="right"><button class="no-print" onclick="window.print();">Cetak Semua</button></div>
 <?php
-session_start();
-error_reporting(0);
 include('../koneksi.php');
-require('../pdf/fpdf.php');
-
-$pdf = new FPDF("P", "cm", array(21.5, 33));
-$pdf->SetMargins(1, 1, 1);
-$pdf->AliasNbPages();
-$pdf->AddPage();
-$pdf->Image('../aset/foto/sugih.jpg', 5, 1, 2, 2);
-$pdf->SetFont('times', 'B', 10);
-$pdf->SetX(4);
-$pdf->MultiCell(15, 0.5, 'PEMERINTAH KABUPATEN CIANJUR', 0, 'C');
-$pdf->SetFont('times', 'B', 14);
-$pdf->SetX(4);
-$pdf->MultiCell(15, 0.5, 'DINAS PERHUBUNGAN', 0, 'C');
-$pdf->SetFont('times', 'B', 9);
-$pdf->SetX(4);
-$pdf->MultiCell(15, 0.5, 'Telepon : (0263) 263424, 2616897 Fax : (0263) 284356', 0, 'C');
-$pdf->SetFont('times', 'B', 9);
-$pdf->SetX(4);
-$pdf->MultiCell(15, 0.5, 'Jl. Dr. Muwardi No. 395, Cianjur, 43215', 0, 'C');
-$pdf->SetX(4);
-$pdf->Line(1, 3.3, 32, 3.3);
-$pdf->SetLineWidth(0.1);
-$pdf->Line(1, 3.4, 32, 3.4);
-$pdf->SetLineWidth(0);
-$pdf->ln(1);
+$queryangkutan = mysqli_query ($konek, "SELECT * FROM angkutan ORDER BY tgl DESC");
+while($row = mysqli_fetch_array($queryangkutan))
+{
+  echo '<table width="800" align="center">';
+  echo '<tr>
+    <td align="justify"><img src="../aset/foto/sugih.jpg" width="100" height="100" /></td>
+    <td colspan="3" align="center">
+      <h3>PEMERINTAH KABUPATEN CIANJUR<br></h3>
+      <h2>DINAS PERUBUNGAN<br></h2>
+      <h3>UPTD TERMINAL PASIR HAYAM<br></h3>
+        Jl. Paris Hayam Kabupaten Cianjur</a></h3>
+    <hr></td>
+  </tr>';
+  echo '<tr>
+    <td colspan="4"><div align="justify">
+      <br>
+      <p>Dasar : <br>
+      1. Undang-undang Republik Indonesia Nomor 22 Tahun 2009 Tentang Lalu Lintas dan Angkutan Jalan; <br>
+      2. Peraturan Pemerintah Republik Indonesia Nomor 37 Tahun 2017 Tentang Keselamatan Lalu Lintas dan Angkutan Jalan.</br>
+      <p><br/>
+      </p>
+    </div></td>
+  </tr>';
+  echo '<tr>
+    <td width="107">&nbsp;</td>
+    <td width="129">No Kendaraan</td>
+    <td colspan="2">: '.$row['noken'].' </td>
+  </tr>';
+  echo '<tr>
+    <td>&nbsp;</td>
+    <td>Nama</td>
+    <td colspan="2">: '.$row['supir'].' </td>
+  </tr>';
+  echo '<tr>
+    <td colspan="4"><div align="justify">
+      <p>&nbsp;</p>
+      <p>Sehubungan dengan tidak lengkapnya kelengkapan kendaraan.....................  <br>
+        Kendaraan initidak diijinkan untuk beriprasi selama kelengkapan tersebut belum terpenuhi.</p>
+        <p>Demikian surat teguran ini dikeluarkan, agar dapat dilaksanakan.</p>
+    </div></td>
+  </tr>';
+  echo '
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td width="223">&nbsp;</td>
+    <td width="321" Align="center">
+    <br>
+    <br>
+    <br>
+    <br>
+    <p>Cianjur,............... <br />
+      Kepala UPTD Terminal<br>
+      Angkutan Umum Pasirhayam</p>
+      <p>&nbsp;</p>
+      <p>  <br />
+        BAMBANG R.S. DALIMUNTHE, SH <br>
+    NIP.19700222 199303 1002</p></td>
+  </tr>';
+echo '</table>
+  <br>
+  <br>';
+  echo '<p style="page-break-after:always;"></p>';
+}
+?>
+</body>
+</html>
